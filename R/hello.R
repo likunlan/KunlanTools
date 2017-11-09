eff8ddad371d10d853c8bf2f2de8076684cd6c4e
likunlan <li.kunlan@campus.lmu.de>
2017-11-09 18:22:13 +0100

    R package update

--- R/hello.R
+++ R/hello.R
@@ -1,61 +1,59 @@
-# height
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-# Script for simple function that checks the difference in height from the sex-
-# specific mean or the overall mean height for each of the students in the given dataframe
-# Date: 02.11.2017
-# Author: likunlan
-
+#Add a welcome text
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("hello, my R world")
-}
+  packageStartupMessage("Welcome to my first R-package and thanks for using it!")}
 
+#' an example function named 'checkheight'
+#' Script for simple function that checks the difference in height from the sex-
+#' specific mean or the overall mean height for each of the students in the given #'dataframe
+#' Date: 02.11.2017
+#' @author Kunlan Li <li.kunlan@campus.lmu.de>
+#' @title checkHeight
+#' @description Script for simple function that checks the difference in height     from the sex-specific mean or the overall mean height for each of the students
 #' @importFrom stats filter
 #' @export
-checkHeight3 = function(students.input, sex.specific = TRUE) {
+#' @param students.input a input dataframe
+#' @param  sex.specific logical value
+#' @param r a vector
+#' @return result of the computation
+#' @example checkHeight(students,TRUE)
+checkHeight = function(students.input, sex.specific = TRUE) {
   #define result.frame
      result.frame = data.frame(matrix(NA, nrow = nrow(students.input), ncol = 2))
      colnames(result.frame) = c("name", "difference")
   #calculate the mean height of man
-  male.mean = students.input %>%
-    filter(sex == "M") %>%
-    summarise(mean = mean(height))
+  male.mean = mymean(students.input[students.input['sex']=='M',][['height']])
+    #students.input %>%
+    #filter(sex == "M") %>%
+    #summarise(mean = mymean(height))
+   #select(height)%>%mymean
   #calculate the mean height of female
-  female.mean = students.input %>%
-    filter(sex == "F") %>%
-    summarise(mean = mean(height))
+  female.mean = mymean(students.input[students.input['sex']=='F',][['height']])
+    #students.input %>%
+    #filter(sex == "F") %>%
+    #summarise(mean = mymean(height))
+  #select(height)%>%mymean
   #calculate the  overall mean height
-  overall.mean = students.input%>%
-    summarise(mean = mean(height))
+  overall.mean = mymean(students.input[,"height"])
+    #students.input%>%
+    #summarise(mean = mymean(height))
+  #select(height)%>%mymean
   #if calculate the difference from the sex-specific
   if(sex.specific==TRUE){
   # use apply  to calculate the difference
   l = apply(students.input,1,function(r){
     if (r['sex'] == "F") {
-      height.diff = 100*(as.numeric(r['height']) - female.mean$mean)
+      height.diff = 100*(as.numeric(r['height']) - female.mean)
     }
     else {
-      height.diff = 100*(as.numeric(r['height']) - male.mean$mean)
+      height.diff = 100*(as.numeric(r['height']) - male.mean)
     }
   }
   )
   }
   #if calculate the difference from the overall mean height
   else{
-    l = apply(students.input,1,function(r){
-      height.diff = 100*(as.numeric(r['height']) - overall.mean$mean)
+    l = apply(students.input, 1, function(r){
+      height.diff = 100*(as.numeric(r['height']) - overall.mean)
     }
       )
   }
@@ -63,4 +61,19 @@ checkHeight3 = function(students.input, sex.specific = TRUE) {
   result.frame[, "difference"]=l
   return(result.frame)
 }
+
+#library(dplyr)
+
+#age = c(19, 22, 21, 23, 22, 20, 28, 25)
+#weight = c(50, 75, 80, 56, 75, 58, 65, 82)
+#height = c(1.66, 1.78, 1.90, 1.72, 1.83, 1.68, 1.70, 1.85)
+#sex = c("F", "M", "M", "F", "M", "F", "F", "M")
+
+#students = data.frame(cbind(age, weight, height, sex))
+#students = transform(students, age = as.numeric(as.character(age)))
+#students = transform(students, height = as.numeric(as.character(height)))
+#students = transform(students, weight = as.numeric(as.character(weight)))S
+
+#students$name = c("Maria", "Franz", "Peter", "Lisa", "Hans", "Eva", "Mia", "Karl")
+#checkHeight(students,TRUE)
 
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,9 @@
-mean <- function(height) {
-  mean(height)
+#' @title mymean file
+#' @description calculate the mean of a input vector
+#' @param v a vector
+#' @return the mean of the vector
+
+mymean <- function(v) {
+  m <- round(sum(v)/length(v), digits = 3)
+  return(m)
 }
